ee1e08dd4b970f2a5135efc7845b49eb9081f75e
Pedro-Arthur13 <parthurmalbuquerque@gmail.com>
2025-03-02 12:38:01 -0300

    Dotfiles

---
+++ fastfetch_launcher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Obtém o tema de ícones atual
+ICON_THEME=$(grep "gtk-icon-theme-name=" ~/.config/gtk-3.0/settings.ini | cut -d'=' -f2)
+
+# Define a pasta de ícones baseada no tema
+if [[ "$ICON_THEME" == "Tela-circle-grey" ]]; then
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/tela-circle-grey"
+else
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/default"
+fi
+
+# Seleciona um PNG aleatório da pasta correspondente
+PNG_FILE=$(find "$PNG_FOLDER" -name "*.png" | shuf -n 1)
+
+# Substitui dinamicamente o valor do logo no JSON de configuração
+jq --arg pngfile "$PNG_FILE" '.logo.source = $pngfile' ~/.config/fastfetch/config.jsonc > ~/.config/fastfetch/config_temp.jsonc
+
+# Executa o Fastfetch com a configuração modificada
+fastfetch --config ~/.config/fastfetch/config_temp.jsonc
