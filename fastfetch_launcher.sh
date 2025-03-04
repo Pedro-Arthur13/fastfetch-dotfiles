4d6d599101b610583014bd3186d2ba51ba6698fc
Pedro-Arthur13 <parthurmalbuquerque@gmail.com>
2025-03-04 20:27:20 -0300

    Sistema mudado, invés de puxar o gtk-icon-theme-name ele puxa apenas o gtk-theme-name, pode acontescer de dois temas terem o mesmo icon-theme; Thema verde e imagens adicionadas

--- fastfetch_launcher.sh
+++ fastfetch_launcher.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Obtém o tema de ícones atual
-ICON_THEME=$(grep "gtk-icon-theme-name=" ~/.config/gtk-3.0/settings.ini | cut -d'=' -f2)
+ICON_THEME=$(grep "gtk-theme-name=" ~/.config/gtk-3.0/settings.ini | cut -d'=' -f2)
 
 # Define a pasta de ícones baseada no tema
 # ${XDG_CONFIG_HOME} eh a variavel de ambiente XDG_CONFIG_HOME, que define onde ficam os arquivos de config
 # do usuario (atraves da especificacao do XDG)
 # ':-' significa se a variavel estiver vazia ou nao definida, usa o valor a direita
 # $HOME/.config eh o padrao caso XDG_CONFIG_HOME nao esteja definiido
-if [[ "$ICON_THEME" == "Tela-circle-grey" ]]; then
-    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/tela-circle-grey"
-elif [[ "$ICON_THEME" == "Tela-circle-dracula" ]]; then
-    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/tela-circle-dracula"
-elif [[ "$ICON_THEME" == "Tela-circle-green" ]]; then
-    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/tela-circle-green"
-elif [[ "$ICON_THEME" == "BeautyLine" ]]; then
-    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/BeautyLine"
+if [[ "$ICON_THEME" == "Graphite-Mono" ]]; then
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/dark"
+elif [[ "$ICON_THEME" == "Catppuccin-Mocha" ]]; then
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/Drakula-Blue"
+elif [[ "$ICON_THEME" == "Decay-Green" ]]; then
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/dark-green"
+elif [[ "$ICON_THEME" == "Material-Sakura" ]]; then
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/verde"
+elif [[ "$ICON_THEME" == "Synth-Wave" ]]; then
+    PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/rosa"
 else
     PNG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}/fastfetch/pngs/default"
 fi
